f6e2c4118acf9bbb0550dfc5141e38e8d3914fce
Vladislav Gorelov <vsg@yandex.ru>
2023-04-01 21:50:31 +0300

    Сделали лаунчскрин и заготовку для интерфейса

---
+++ Joke Generator/Helpers/UIColor+Extension.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension UIColor {
+        static var jkGreen: UIColor { UIColor(named: "JokeGreen") ?? UIColor.green }
+        static var jkDarkRed: UIColor { UIColor(named: "JokeDarkRed") ?? UIColor.red }
+        static var jkBlack: UIColor { UIColor(named: "JokeBlack") ?? UIColor.black}
+        static var jkBtnGreen: UIColor { UIColor(named: "JokeButtonGreen") ?? UIColor.green }
+        static var jkPink: UIColor { UIColor(named: "JokePink") ?? UIColor.magenta }
+        static var jkWhite: UIColor { UIColor(named: "JokeWhite") ?? UIColor.white}
+     }
