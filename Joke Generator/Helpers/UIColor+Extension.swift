6203f9487d3cf4464677ed302ed1c87f3d4409a9
Vladislav Gorelov <vsg@yandex.ru>
2023-04-02 11:23:36 +0300

    Only one button, one press and get new random joke from web

--- Joke Generator/Helpers/UIColor+Extension.swift
+++ Joke Generator/Helpers/UIColor+Extension.swift
@@ -4,7 +4,7 @@ extension UIColor {
         static var jkGreen: UIColor { UIColor(named: "JokeGreen") ?? UIColor.green }
         static var jkDarkRed: UIColor { UIColor(named: "JokeDarkRed") ?? UIColor.red }
         static var jkBlack: UIColor { UIColor(named: "JokeBlack") ?? UIColor.black}
-        static var jkBtnGreen: UIColor { UIColor(named: "JokeButtonGreen") ?? UIColor.green }
+    static var jkBtnGreen: UIColor { UIColor(named: "JokeButtonGreen") ?? UIColor.purple }
         static var jkPink: UIColor { UIColor(named: "JokePink") ?? UIColor.magenta }
         static var jkWhite: UIColor { UIColor(named: "JokeWhite") ?? UIColor.white}
      }
--- Joke Generator/Presentation/ViewController.swift
+++ Joke Generator/Presentation/ViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var storyTextView: UITextView!
+    @IBOutlet weak var punchlineButton: UIButton!
+
+
+
+
+    @IBAction func giveMeAJoke(_ sender: Any) {
+        let apiUrl = URL(string: "https://official-joke-api.appspot.com/random_joke")!
+            URLSession.shared.dataTask(with: apiUrl) { data, response, error in
+                guard let data = data, error == nil else {
+                    print("Error fetching joke: \(error?.localizedDescription ?? "Unknown error")")
+                    return
+                }
+                do {
+                    let joke = try JSONDecoder().decode(Joke.self, from: data)
+                    DispatchQueue.main.async {
+                        self.storyTextView.text = joke.setup + "\n\n" + joke.punchline
+                    }
+                } catch {
+                    print("Error decoding joke: \(error.localizedDescription)")
+                }
+            }.resume()
+
+}
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        punchlineButton.tintColor = UIColor.jkGreen
+        getJoke()
+    }
+
+    func getJoke() {
+        let apiUrl = URL(string: "https://official-joke-api.appspot.com/random_joke")!
+        URLSession.shared.dataTask(with: apiUrl) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Error fetching joke: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            do {
+                let joke = try JSONDecoder().decode(Joke.self, from: data)
+                DispatchQueue.main.async {
+                    self.storyTextView.text = joke.setup + "\n\n" + joke.punchline
+                }
+            } catch {
+                print("Error decoding joke: \(error.localizedDescription)")
+            }
+        }.resume()
     }
 
 
+
+
+
+} // конец класса
+
+struct TranslationText: Codable {
+    let text: String
 }
+
+struct Translation: Codable {
+    let code: Int
+    let lang: String
+    let text: [String]
+}
+
+struct Joke: Codable {
+    let setup: String
+    let punchline: String
+}
+
+
+
+
 
